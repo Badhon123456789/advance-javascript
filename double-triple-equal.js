768b268f56f1d98d9bc3ffb0796e1416be3cd271
Badhon123456789 <mushfiqurr15@gmail.com>
2023-03-09 23:44:30 +0600

    double and triple equal

--- double-triple-equal.js
+++ double-triple-equal.js
@@ -1,2 +1,84 @@
-// ==
-// ===
+// ***==(comparing just value)
+// ***===(comparing value and also type)
+
+// let first = 2;
+// let second = "2";
+// if (first == second) {
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let first = 2;
+// let second = 2;
+// if (first == second) {
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let first = 2;
+// let second = "2";
+// if (first == second) {
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let first = 2;
+// let second = "2";
+// if (first === second) {
+//     console.log("Condition is true");
+// }
+// else{
+// console.log("Condition is false");
+// }
+        
+
+//**/ we know[0 = false & 1 = true]**
+        
+// let first = 1;
+// let second = true;
+// if (first == second) {
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// let first = 1;
+// let second = true;
+// if (first === second) {
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let first = 0;
+// let second = false;
+// if (first == second) {
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+let first = 1;
+let second = true;
+if (first === second) {
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
